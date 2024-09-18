448d12f706e0974da4ee0c6153259eda3dfe2ddd
Ragulprasanth3333 <lonewolf06111999@gmail.com>
2024-09-18 16:54:24 +0530

    modified

--- Desktop/proffesional/Ardunio/buzz.ino
+++ Desktop/proffesional/Ardunio/buzz.ino
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 
 int buzzPin = 8;
-String prompt = "Enter 1 to buzz, 0 to stop: ";
+String prompt = "Enter a number (1-5) to buzz, 0 to stop: ";
 int inputNum;
 
 void setup() {
@@ -14,16 +14,19 @@ void loop() {
   if (Serial.available() > 0) {
     inputNum = Serial.parseInt();
     
-    if (inputNum == 1) {
-      digitalWrite(buzzPin, HIGH);
-      delay(2000);
-      digitalWrite(buzzPin, LOW);
+    if (inputNum >= 1 && inputNum <= 5) {
+      for (int i = 0; i < inputNum; i++) {
+        digitalWrite(buzzPin, HIGH);
+        delay(500);
+        digitalWrite(buzzPin, LOW);
+        delay(500);
+      }
     } 
     else if (inputNum == 0) {
       digitalWrite(buzzPin, LOW);
     } 
     else {
-      Serial.println("Invalid input. Try again.");
+      Serial.println("Invalid input. Enter a number between 1 and 5.");
     }
     
     Serial.println(prompt);
