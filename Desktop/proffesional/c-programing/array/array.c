ca29b32775935c380233a69757dc80f26672bbec
Ragulprasanth3333 <lonewolf06111999@gmail.com>
2024-10-08 06:24:40 +0530

    modified

--- Desktop/proffesional/c-programing/array/array.c
+++ Desktop/proffesional/c-programing/array/array.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
 
 int main() {
-    int arr[50],i,size;
-    printf("enter the size of arr :");
-    scanf("%d",&size);
-    if (size > 50)
-    {
-        printf("it is out of range");
-    }
-    else if (size < 50)
-    {
-        for(i=0;i<size;i+=1)
-        {
-            printf("Enter the element :");
-            scanf("%d",&arr[i]);
-        }
-        for(i = 0;i<size;i+=1)
-        {
-            printf("%d ",arr[i]);
-        }
+    int arr[50], size, i;
 
-    }    
+    // Input array size
+    printf("Enter the size of the array (max 50): ");
+    scanf("%d", &size);
+
+    // Input array elements
+    printf("Enter %d elements of the array:\n", size);
+    for (i = 0; i < size; i++) {
+        printf("Element %d: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+
+    // Display array elements
+    printf("Array elements: ");
+    for (i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
 }
