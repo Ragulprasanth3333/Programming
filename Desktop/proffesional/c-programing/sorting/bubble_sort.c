c803d3bc5218f7d7fd139fe1f3f5a8fb80aee031
Ragulprasanth3333 <lonewolf06111999@gmail.com>
2024-11-04 11:32:57 +0530

    done

---
+++ Desktop/proffesional/c-programing/sorting/bubble_sort.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define n 5
+
+int arr[n] = {15, 16, 6, 8, 5};
+
+void main() {
+    int i, j, temp;
+    int k = n - 1;
+
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < k; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap arr[j] and arr[j + 1]
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+        k -= 1; // Decrement k after each pass
+    }
+
+    // Print sorted array
+    printf("Sorted array: ");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
