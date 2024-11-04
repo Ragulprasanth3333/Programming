ac7141b9ba4133a55322ef0724c53ea3187c6101
Ragulprasanth3333 <lonewolf06111999@gmail.com>
2024-11-04 17:29:48 +0530

    optimised

--- Desktop/proffesional/c-programing/sorting/bubble_sort.c
+++ Desktop/proffesional/c-programing/sorting/bubble_sort.c
@@ -5,17 +5,22 @@
 int arr[n] = {15, 16, 6, 8, 5};
 
 void main() {
-    int i, j, temp;
+    int i, j, temp, flag;
     int k = n - 1;
 
     for (i = 0; i < n - 1; i++) {
+        flag = 0;
         for (j = 0; j < k; j++) {
             if (arr[j] > arr[j + 1]) {
                 // Swap arr[j] and arr[j + 1]
                 temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
+                flag = 1; // Indicate a swap occurred
             }
+        }
+        if (flag == 0) {
+            break; // Exit early if no swaps were made
         }
         k -= 1; // Decrement k after each pass
     }
